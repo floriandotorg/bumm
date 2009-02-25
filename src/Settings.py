ab6d2231b9e45f00eb969f5addd7333de629aa5d
BFlader <BFlader@4b88618a-de2d-11dd-a8eb-ddf07f3c1838>
2009-02-25 07:29:36 +0000

    Implementation Settings

--- src/Settings.py
+++ src/Settings.py
@@ -27,42 +27,165 @@
 
 from PyQt4 import QtCore
 
-## Verwaltet Programmeinstellungen und speichert diese in einer .ini Datei.
-# Diese Datei hat immer den Namen "config.ini" und liegt Verzeichnis für 
-# Einstellugnen des Betriebsystems (z.B. Dokumente und Einstellungen)
-# Bei der Datei handelt sich um ein Standard-Windows-Konfigurationsfile.
-# Sollte sich diese Datei am angegebenen Ort befinden, wird sie gelesen,
-# anderenfalls wird eine neue Datei mit Standardwerten gefüllt.
+## Verwaltet Programmeinstellungen und speichert diese je nach System.
+#  Ist noch keine Datei vorhanden werden die Einstellungen mit Default-Werten
+#  gefüllt.
 class Settings(QtCore.QObject):
-    
+
     ## Konstruktor
     # @param p_parent Übergeordnetes QObject
     def __init__(self, p_parent = None):
+        # initialisiere Objekt
         QtCore.QObject.__init__(self, p_parent)
-        self._username = "ChuckNoris"
-        
-        
-        
-        self.server_address = "http://10.200.132.22/bscw/bscw.cgi/?op=xmlrpc"
-        self.columns = ["user_id", "name", "longname"]
-        self.login_dialog_geometry = QtCore.QRect()
-        self.col_dialog_geometry = QtCore.QRect()
-        self.main_window_geometry = QtCore.QRect()
-        self.user_details_geometry = QtCore.QRect()
-        self.show_user_details = True
-        
-    # Getter/Setter Beispiel für Username
+
+        # initialisiere QSettings-Objekt
+        self._settings = QtCore.QSettings("BUMM")
+
+        # lege Standardwerte fest
+        self.__username = ""
+        self.__server_address = ""
+        self.__columns = ["user_id", "name", "longname"]
+        self.__login_dialog_geometry = QtCore.QRect()
+        self.__col_dialog_geometry = QtCore.QRect()
+        self.__main_window_geometry = QtCore.QRect()
+        self.__user_details_geometry = QtCore.QRect()
+        self.__show_user_details = True
+
+        # lese Einstellungen aus
+        self._username = self._settings.value("username",
+                            QtCore.QVariant(self.__username)).toString()
+        self._server_address = self._settings.value("server_address",
+                            QtCore.QVariant(self.__server_address)).toList()
+        self._columns = self._settings.value("columns",
+                            QtCore.QVariant(self.__columns)).toString()
+        self._login_dialog_geometry = self._settings.value("login_dialog_geometry",
+                            QtCore.QVariant(self.__login_dialog_geometry)).toRect()
+        self._col_dialog_geometry = self._settings.value("col_dialog_geometry",
+                            QtCore.QVariant(self.__col_dialog_geometry)).toRect()
+        self._main_window_geometry = self._settings.value("main_window_geometry",
+                            QtCore.QVariant(self.__main_window_geometry)).toRect()
+        self._user_details_geometry = self._settings.value("user_details_geometry",
+                            QtCore.QVariant(self.__user_details_geometry)).toRect()
+        self._show_user_details = self._settings.value("show_user_details",
+                            QtCore.QVariant(self.__show_user_details)).toBool()
+
+    ## Dekonstruktor
+    def __del__(self):
+        self._settings.setValue("username", QtCore.QVariant(self._username))
+        self._settings.setValue("server_address",
+                            QtCore.QVariant(self._server_address))
+        self._settings.setValue("columns",
+                            QtCore.QVariant(self._columns))
+        self._settings.setValue("login_dialog_geometry",
+                            QtCore.QVariant(self._login_dialog_geometry))
+        self._settings.setValue("col_dialog_geometry",
+                            QtCore.QVariant(self._col_dialog_geometry))
+        self._settings.setValue("main_window_geometry",
+                            QtCore.QVariant(self._main_window_geometry))
+        self._settings.setValue("user_details_geometry",
+                            QtCore.QVariant(self._user_details_geometry))
+        self._settings.setValue("show_user_details",
+                            QtCore.QVariant(self._show_user_details))
+
+        # Daten speichern
+        self._settings.sync()
+
+    # Get'er
+
+    ## gibt den Benutzernamen zurück
+    # @return Benutzername als QString
     def getUsername(self):
-        print "getter"
         return self._username
-    
-    def setUsername(self, username):
-        print "setter"
-        self._username = username
-        
+
+    ## gibt die Serveradresse zurück
+    # @return Serveradresse als QString
+    def getServerAddress(self):
+        return self._server_address
+
+    ## gibt die Spaltennamen zurück
+    # @return Spaltennamen als Liste
+    def getColumns(self):
+        return self._columns
+
+    ## gibt die geometrischen Daten des Login-Dialogs zurück
+    # @return geometrische Daten des Login-Dialogs als QRect
+    def getLoginDialogGeometry(self):
+        return self._login_dialog_geometry
+
+    ## gibt die geometrischen Daten des Spalten-Dialogs zurück
+    # @return geometrische Daten des Spalten-Dialogs als QRect
+    def getColDialogGeometry(self):
+        return self._col_dialog_geometry
+
+    ## gibt die geometrischen Daten des MainWindow-Dialogs zurück
+    # @return geometrische Daten des MainWindow-Dialogs als QRect
+    def getMainWindowGeometry(self):
+        return self._main_window_geometry
+
+    ## gibt die geometrischen Daten des UserDetail-Dialogs zurück
+    # @return geometrische Daten des Login-Dialogs als QRect
+    def getUserDetailsGeometry(self):
+        return self._user_details_geometry
+
+    ## gibt den Boolean zurück ob UserDetails angezeigt werden sollen
+    #  zurück
+    # @return UserDetails als Boolean
+    def getShowUserDetails(self):
+        return self._show_user_details
+
+    # Set'er
+
+    ## setzt den Benutzernamen
+    # @param p_username Benutzername
+    def setUsername(self, p_username):
+        self._username = p_username
+
+    ## setzt die Serveradresse
+    # @param p_server_adress Serveradresse als QString
+    def setServerAddress(self, p_server_address):
+        self._server_address = p_server_adress
+
+    ## setzt die Spaltennamen
+    # @param p_columns Spaltennamen als Liste
+    def setColumns(self, p_columns):
+        self._columns = p_columns
+
+    ## setzt die geometrischen Daten des Login-Dialogs
+    # @param p_login_dialog_geometry Daten des Login-Dialogs als QRect
+    def setLoginDialogGeometry(self):
+        self._login_dialog_geometry = p_login_dialog_geometry
+
+    ## setzt die geometrischen Daten des Spalten-Dialogs
+    # @param p_col_dialog_geometry geometrische Daten des Spalten-Dialogs als QRect
+    def setColDialogGeometry(self, p_col_dialog_geometry):
+        self._col_dialog_geometry = p_col_dialog_geometry
+
+    ## setzt die geometrischen Daten des MainWindow-Dialogs
+    # @param p_main_window_geometry geometrische Daten des MainWindow-Dialogs als QRect
+    def setMainWindowGeometry(self, p_main_window_geometry):
+        self._main_window_geometry = p_main_window_geometry
+
+    ## setzt die geometrischen Daten des UserDetail-Dialogs
+    # @param p_user_details_geometry geometrische Daten des Login-Dialogs als QRect
+    def setUserDetailsGeometry(self, p_user_details_geometry):
+        self._user_details_geometry = p_user_details_geometry
+
+    ## setzt den Boolean ob UserDetails angezeigt werden sollen
+    # @param _show_user_details UserDetails als Boolean
+    def setShowUserDetails(self, p_show_user_details):
+        self._show_user_details = p_show_user_details
+
+    # Propertys
     username = property(getUsername, setUsername)
-    
+    server_address = property(getServerAddress, setServerAddress)
+    columns = property(getColumns, setColumns)
+    login_dialog_geometry = property(getLoginDialogGeometry, setLoginDialogGeometry)
+    col_dialog_geometry = property(getColDialogGeometry, setColDialogGeometry)
+    main_window_dialog_geometry = property(getMainWindowGeometry, setMainWindowGeometry)
+    user_details_geometry = property(getUserDetailsGeometry, setUserDetailsGeometry)
+    show_user_details = property(getShowUserDetails, setShowUserDetails)
+
 if __name__ == "__main__":
     s = Settings()
+    s.setUsername("Flo")
     print s.username
-    
